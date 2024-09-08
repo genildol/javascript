4eafb337af093ac0e898faae8624aff4be162c9b
Genildo Souza <jgmicroshs@gmail.com>
2024-09-08 11:17:15 -0300

    classes Action JS

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 class Person {
 
   age = 0;
+  steps = 0;
 
   constructor(name) {
     this.name = name;
   }
 
-  // constructor(name, age) {
-  //   this.name = name;
-  //   this.age = age;
-}
+  takeAStep() {
 
+    this.steps++;
+
+  }
+  setAge(newAge) {
+    if (typeof newAge == 'number') {
+      this.age = newAge
+
+    } else {
+      console.log("Não é um número");
+    }
+
+  }
+}
 
 let p1 = new Person("João");
 let p2 = new Person("Maria");
 let p3 = new Person("Pedro");
 
 
-p1.age = 20;
+p1.setAge(35);
 
-console.log(`${p1.name} tem ${p1.age} anos`);
+console.log(`${p1.name} tem ${p1.age} anos.`);
 
-// console.log(p3);
+
+// p1.takeAStep();
+// p2.takeAStep();
+// console.log(`Passos de ${p1.name}: ${p1.steps}`);
+// console.log(`Passos de ${p2.name}: ${p2.steps}`);
